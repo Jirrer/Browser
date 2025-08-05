0ceacf7ed6d7c7d4e4e6b4779213f1da513bacfe
John Irrer <jrirrer@gmail.com>
2025-08-05 14:18:37 -0400

    created browser

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("     ___Browser Name___     ");
+        Console.WriteLine("|----------------------------------------------------------------------------------------------------------------|");
+        Console.WriteLine("|                                                                                                                |");
+
+        int inputY = Console.CursorTop;
+        int inputX = 3;
+
+        Console.WriteLine("|----------------------------------------------------------------------------------------------------------------|");
+
+        Console.SetCursorPosition(inputX, inputY - 2); 
+
+        string name = Console.ReadLine();
+
+      
+    }
+}
